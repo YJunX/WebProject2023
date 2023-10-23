65cbb678801d58599370cada7a2eb7a19971b53c
YJunX <xushen006@gmail.com>
2023-10-24 10:35:25 +1300

    add the yugioh api in but is too much data i need to fix later

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,24 +1,45 @@
 <!-- src/App.svelte -->
 <script>
-    // Sample card data
-    let cards = [
-        {
-            name: 'Dark Magician',
-            type: 'Spellcaster',
-            level: 7,
-            attack: 2500,
-            defense: 2100
-        },
-        {
-            name: 'Blue-Eyes White Dragon',
-            type: 'Dragon',
-            level: 8,
-            attack: 3000,
-            defense: 2500
-        },
-        // Add more card data as needed
-    ];
+    import { onMount } from "svelte";
+
+    // Define an array to store the card data from the API
+    let Cards = [];
+
+    // Function to fetch card data from the API
+    async function fetchCardData() {
+        try {
+            const response = await fetch(
+                "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+            );
+            if (response.ok) {
+                const data = await response.json();
+                // Assuming the API response is an array of cards
+                Cards = data.data;
+            } else {
+                console.error("Failed to fetch data from the API");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    // Fetch card data when the component is mounted
+    onMount(fetchCardData);
 </script>
+
+<main>
+    <h2>Latest Cards</h2>
+    {#each Cards as card (card.id)}
+        <!-- Assuming each card has a unique ID -->
+        <div class="card">
+            <h3>{card.name}</h3>
+            <p>Type: {card.type}</p>
+            <p>Level: {card.level}</p>
+            <p>Attack: {card.attack}</p>
+            <p>Defense: {card.defense}</p>
+        </div>
+    {/each}
+</main>
 
 <style>
     /* Add your CSS styles here */
@@ -30,19 +51,3 @@
         border-radius: 5px;
     }
 </style>
-
-
-<main>
-    <h2>Latest Cards</h2>
-    {#each cards as card}
-        <div class="card">
-            <h3>{card.name}</h3>
-            <p>Type: {card.type}</p>
-            <p>Level: {card.level}</p>
-            <p>Attack: {card.attack}</p>
-            <p>Defense: {card.defense}</p>
-        </div>
-    {/each}
-</main>
-
-
