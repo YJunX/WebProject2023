1156176544f2e8da21064691860812ca14711f7f
YJunX <xushen006@gmail.com>
2023-10-30 10:04:48 +1300

    add image in and some text

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -9,10 +9,21 @@
             <p>Kazuo Takahashi, known professionally as Kazuki Takahashi, was a Japanese manga artist. He made his serial manga debut in 1986, and is best known as the author of Yu-Gi-Oh!, published in Weekly Shōnen Jump from 1996 to 2004.</p>
         </div>
         <div class="image">
-            <img src="your-image.jpg" alt="Your Image">
+            <img src="creator.png" alt="Creator image">
         </div>
     </div>
 </main>
 <style>
-	
+	.About-container {
+            display: flex; /* Use flexbox to arrange elements */
+            align-items: center; /* Vertically center the content */
+        }
+
+        .text {
+            flex: 1; /* Allow the text to take up available space */
+        }
+
+        .image {
+            margin-left: 20px; /* Add margin to create space between text and image */
+        }
 </style>
