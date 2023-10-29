7b37b2fb11b10f72898c2bc422904a2cf2a91922
YJunX <xushen006@gmail.com>
2023-10-30 09:55:22 +1300

    add the creator in and some text

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -3,7 +3,15 @@
 </script>
 
 <main>
-	
+	<div class="About-container">
+        <div class="text">
+            <h1>Kazuki Takahashi</h1>
+            <p>Kazuo Takahashi, known professionally as Kazuki Takahashi, was a Japanese manga artist. He made his serial manga debut in 1986, and is best known as the author of Yu-Gi-Oh!, published in Weekly Shōnen Jump from 1996 to 2004.</p>
+        </div>
+        <div class="image">
+            <img src="your-image.jpg" alt="Your Image">
+        </div>
+    </div>
 </main>
 <style>
 	
