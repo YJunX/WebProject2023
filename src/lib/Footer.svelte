6f46690251b5d3309c24d7336f3c09cdead998f7
YJunX <xushen006@gmail.com>
2023-11-09 20:41:36 +1300

    add the css in

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -17,42 +17,53 @@
       background-color: #B8B3B6;
       margin-top: auto;
     }
-    
-    .container {
-      max-width: 960px;
-      margin: 0 auto;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-    .copyright{
-      background-color: white;
-      font-size: smaller;
-      max-width: 1950px;
-      margin-top: 50px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-    .links {
-      margin-top: 10px;
-      display: flex;
-      color: #666;
-    }
-    a {
+
+  .footer a {
+      color: #fff; 
       text-decoration: none;
-      margin-right: 100px;
-    }
-    a:hover {
-      text-decoration: underline;
-    }
-    a:visited {
-        text-decoration: underline;
-        color: rgb(14, 13, 13);
-    }
-    .hr{
-      border: 1px solid black;
-    }
-  </style>
+  }
+
+  .footer-logo-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 10px;
+  }
+
+  .footer-logo {
+      width: 100px;
+  }
+
+  .info-container {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+  }
+
+  .column {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+  }
+
+  .informations-list {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+  }
+
+  .informations-list p {
+      margin: 5px 0;
+  }
+
+  .copyright {
+      font-size: smaller;
+      max-width: 100%;
+      margin-top: 50px;
+      text-align: center;
+  }
+  .copyright p{
+    margin: 0;
+  }
+</style>
