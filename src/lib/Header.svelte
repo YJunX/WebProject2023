0aa65a47e3bd0da86e01a74ff4023e58f91542d1
YJunX <xushen006@gmail.com>
2023-11-10 17:44:21 +1300

    add commenting for code

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,11 +3,20 @@
 </div>
 
 
-<style>
+ <!-- CSS Styles -->
+ <style>
+    /* Style for the background image container */
     .background-image-container {
+        /* Set the background image URL */
         background-image: url('yugioh-games.jpeg');
+        /* Cover the entire container with the background image */
         background-size: cover;
+        /* Center the background image */
         background-position: center;
-        height: 100vh; /* Adjust this to your preferred height */
+        /* Set the container height to 100% of the viewport height */
+        height: 100vh;
+        /* Add any additional styling as needed */
+        /* For example, you might want to add some padding or other styles */
+        /* padding: 20px; */
     }
 </style>
